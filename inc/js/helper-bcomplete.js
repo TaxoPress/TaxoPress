4d5fd06d0fc9d892008c577b0b1818bb4a816869
momo360modena <momo360modena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-28 08:16:13 +0000

    remove token input, remove bcomplete
    git-svn-id: http://plugins.svn.wordpress.org/simple-tags/trunk@198967 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/js/helper-bcomplete.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-	if ( document.getElementById('adv-tags-input') ) {
-		var tags_input = new BComplete('adv-tags-input');
-		tags_input.setData(collection);
-	}
-});
