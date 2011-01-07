42060a49e18e4c021d7a1c2afe80328c82de01a3
momo360modena <momo360modena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-07 12:51:08 +0000

    add option to uninstaller
    git-svn-id: http://plugins.svn.wordpress.org/simple-tags/trunk@329715 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -40,11 +40,14 @@ function SimpleTags_Install() {
  */
 function SimpleTags_Uninstall() {
 	// Delete options
-	delete_option( STAGS_OPTIONS_NAME );
-	delete_option( STAGS_OPTIONS_NAME . '-version' );
+	delete_option( STAGS_OPTIONS_NAME ); // Options plugin
+	delete_option( STAGS_OPTIONS_NAME . '-version' ); // Version ST
+	
+	delete_option( STAGS_OPTIONS_NAME_AUTO ); // Options auto tags
+	delete_option( 'tmp_auto_tags_st' ); // Autotags Temp
+		
 	delete_option( 'stp_options' ); // Old options from Simple Tagging !
-	delete_option( 'widget_stags_cloud' );
-	delete_option( 'tmp_auto_tags_st' );
+	delete_option( 'widget_stags_cloud' ); // Widget
 	
 	// Init roles
 	if ( function_exists('get_role') ) {
