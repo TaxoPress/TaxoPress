9764e1b392b5d7048da8e2d5ee054397d215c9ea
Amaury Balmer <amaury@beapi.fr>
2013-06-18 22:39:47 +0200

    revert changes

--- inc/class.client.autolinks.php
+++ inc/class.client.autolinks.php
@@ -26,17 +26,6 @@ class SimpleTags_Client_Autolinks {
 	 * @return array
 	 */
 	public static function the_posts( $posts, $query ) {
-		// Default value
-		$query->query_vars['post_type'] = empty($query->query_vars['post_type']) ? 'post' : $query->query_vars['post_type'];
-		
-		if ( 
-			!$query->is_main_query() 
-			|| ( isset($query->query_vars['post_type']) && is_string($query->query_vars['post_type']) && $query->query_vars['post_type'] != 'post' ) 
-			|| ( isset($query->query_vars['post_type']) && is_array($query->query_vars['post_type']) && in_array('post', $query->query_vars['post_type']) ) 
-			) {
-			return $posts;
-		}
-		
 		if ( !empty($posts) && is_array($posts) ) {
 			foreach( (array) $posts as $post) {
 				self::$posts[] = (int) $post->ID;
