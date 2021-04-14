cee7f2a4fd0bc6aa9b9e6d1b9976a1eb839729af
Olawale Adesina <olawale.olatech.pro@gmail.com>
2021-04-14 19:52:17 +0100

    - Show delete/deactivate tab only when editing taxonomy

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -397,11 +397,12 @@ class SimpleTags_Admin_Taxonomies
                                                 <a href="#taxonomy_advanced"><span><?php esc_html_e('Advanced',
                                                             'simpletags'); ?></span></a>
                                             </li>
-
+                                            <?php if (!empty($_GET) && !empty($_GET['action']) && 'edit' === $_GET['action']) { ?>
                                             <li class="taxonomy_delete_tab" data-content="taxonomy_delete">
                                                 <a href="#taxonomy_delete"><span><?php esc_html_e('Deactivate or Delete',
                                                             'simpletags'); ?></span></a>
                                             </li>
+                                            <?php } ?>
                                         </ul>
 
 
