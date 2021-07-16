38684a9a2285c59810ef85e9e66ec7b37234dcfd
Olawale Adesina <olawale.olatech.pro@gmail.com>
2021-07-16 18:29:45 +0100

    - Fixed: Terms display showing more than shortcode maximum value close #653

--- inc/tag-clouds-functions.php
+++ inc/tag-clouds-functions.php
@@ -317,8 +317,8 @@ function taxopress_termsdisplay_shortcode($atts)
 
         ob_start();
         if (array_key_exists($tagcloud_id, $tagclouds)) {
+            $tagclouds[$tagcloud_id]['number'] = $tagclouds[$tagcloud_id]['max'];
             $tagcloud_arg = build_query($tagclouds[$tagcloud_id]);
-
             echo SimpleTags_Client_TagCloud::extendedTagCloud( $tagcloud_arg );
 
         } else {
