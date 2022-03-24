97cfc7269225037ca70e05e1cceb2c8eab8de951
ojopaul <ojopaul.dev@gmail.com>
2022-03-24 08:02:57 -0700

    Add "View" links for taxonomies #1334

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -1675,6 +1675,14 @@ if (!empty($_GET) && !empty($_GET['action']) && 'edit' === $_GET['action']) { ?>
 
                                                 </div>';
                         }
+                        $taxonomy_url =  add_query_arg(
+                            [
+                                'taxonomy' => $current["name"],
+                            ],
+                            taxopress_admin_url('edit-tags.php')
+                        );
+                        echo '<br /><a href="'. esc_url($taxonomy_url).'" class="button"> ' . esc_html__('View Taxonomy',
+                        'simple-tags') . '</a>';
                     }
                         ?>
 
