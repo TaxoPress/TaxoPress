20031ca0591cbd62919be7d3632b8345d512e3d6
Adesina Adedeji Felix <DEJI@Adesinas-MacBook-Pro.local>
2024-08-16 17:00:24 +0100

    updated the header for manage terms

--- inc/class.admin.manage.php
+++ inc/class.admin.manage.php
@@ -130,9 +130,9 @@ class SimpleTags_Admin_Manage
 <div class="wrap st_wrap tagcloudui st-manage-terms-page admin-settings wrap st_wrap">
 	<?php SimpleTags_Admin::boxSelectorTaxonomy('st_manage'); ?>
 
-	<h2><?php _e('Manage Terms', 'simple-tags'); ?>
-	</h2>
-    <div class="manageterms-description"><?php esc_html_e('This feature allows you to manage your content terms by adding, renaming, merging and deleting unused terms.', 'simple-tags'); ?></div>
+	<h1><?php _e('Manage Terms', 'simple-tags'); ?>
+	</h1>
+    <div class="taxopress-description"><?php esc_html_e('This feature allows you to manage your content terms by adding, renaming, merging and deleting unused terms.', 'simple-tags'); ?></div>
 
 	<div class="clear"></div>
 
