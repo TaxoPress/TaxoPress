8c2805c4875d07a62c07f46d1285168dd34d1f7a
Deji98 <adesinafelix98@gmail.com>
2024-11-12 01:38:04 +0100

    added new display format tab

--- inc/class.admin.php
+++ inc/class.admin.php
@@ -505,6 +505,12 @@ class SimpleTags_Admin
 			'save_settings' => esc_html__('Auto Term ID missing. Kindly save the auto term before using this feature.', 'simple-tags'),
 			'delete_label' => esc_html__('Delete', 'simple-tags'),
 			'ai_nonce' => wp_create_nonce('taxopress-ai-ajax-nonce'),
+			//we want to pass these as placeholders so no need to translate
+			'post_title'              => '%post_title%',
+			'post_permalink'          => '%post_permalink%',
+			'post_date'               => '%post_date%',
+			'post_thumb_url'          => '%post_thumb_url%',
+			'post_category'           => '%post_category%',
 		]);
 
 
