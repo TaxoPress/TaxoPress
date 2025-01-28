bcbdd853246ff3add1f54a1225bbb32338f8cd7e
Ojo Paul <ojopaul.dev@gmail.com>
2025-01-28 09:15:32 +0100

    Put some Metabox features only in Pro #2441

--- inc/helper.options.admin.php
+++ inc/helper.options.admin.php
@@ -18,13 +18,6 @@ foreach (TaxoPressAiUtilities::get_post_types_options() as $post_type => $post_t
         }
     }
 
-    $default_taxonomy_display_options = [
-        'default' => esc_html__('Default', 'simple-tags'),
-        'dropdown' => esc_html__('Dropdown', 'simple-tags'),
-        'radio' => esc_html__('Radio', 'simple-tags'),
-        'checkbox' => esc_html__('Checkbox', 'simple-tags'),
-    ];
-
     if (empty($default_taxonomy_options)) {
         // This feature only matter if a post has taxonomy
         $taxopress_ai_fields[] = array(
@@ -64,15 +57,25 @@ foreach (TaxoPressAiUtilities::get_post_types_options() as $post_type => $post_t
             'taxopress-ai-tab-content-sub taxopress-ai-'. $post_type .'-content-sub enable_taxopress_ai_' . $post_type . '_metabox_field st-subhide-content'
             );
 
-        // add taxonomy display option
-        $taxopress_ai_fields[] = array(
-            'taxopress_ai_' . $post_type . '_metabox_display_option',
-            '<div class="taxopress-ai-tab-content-sub taxopress-settings-subtab-title taxopress-ai-'. $post_type .'-content-sub enable_taxopress_ai_' . $post_type . '_metabox_field st-subhide-content">' . esc_html__('Metabox Taxonomy Display', 'simple-tags') . '</div>',
-            'select',
-            $default_taxonomy_display_options,
-            '',
-            'taxopress-ai-tab-content-sub taxopress-ai-'. $post_type .'-content-sub enable_taxopress_ai_' . $post_type . '_metabox_field st-subhide-content'
-        );
+        if (taxopress_is_pro_version()) {
+
+            $default_taxonomy_display_options = [
+                'default' => esc_html__('Default', 'simple-tags'),
+                'dropdown' => esc_html__('Dropdown', 'simple-tags'),
+                'radio' => esc_html__('Radio', 'simple-tags'),
+                'checkbox' => esc_html__('Checkbox', 'simple-tags'),
+            ];
+            
+            // add taxonomy display option
+            $taxopress_ai_fields[] = array(
+                'taxopress_ai_' . $post_type . '_metabox_display_option',
+                '<div class="taxopress-ai-tab-content-sub taxopress-settings-subtab-title taxopress-ai-'. $post_type .'-content-sub enable_taxopress_ai_' . $post_type . '_metabox_field st-subhide-content">' . esc_html__('Metabox Taxonomy Display', 'simple-tags') . '</div>',
+                'select',
+                $default_taxonomy_display_options,
+                '',
+                'taxopress-ai-tab-content-sub taxopress-ai-'. $post_type .'-content-sub enable_taxopress_ai_' . $post_type . '_metabox_field st-subhide-content'
+            );
+        }
 
         // add feature tab
         $tab_field_options = [];
