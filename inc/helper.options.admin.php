9ccdbbc77ff6f9bd37ad5883adfd21fa72bb5ed3
deji98 <deji98.adesinafelix98@gmail.com>
2025-03-18 14:44:19 -0700

    disabled the hidden terms feature by default

--- inc/helper.options.admin.php
+++ inc/helper.options.admin.php
@@ -272,7 +272,7 @@ return apply_filters('taxopress_admin_options', array(
                 'enable_hidden_terms',
                 __('Enable Hidden Terms:', 'simple-tags'),
                 'checkbox',
-                '0',
+                '1',
                 __('This feature will hide terms that are infrequently used. These terms will be visible inside the WordPress admin area, but not on the front of this site.', 'simple-tags'),
                 ''
             ),
