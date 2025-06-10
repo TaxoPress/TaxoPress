53cf695fa52eb2c4b1137eb93082db4ea9cb6d99
deji98 <deji98.adesinafelix98@gmail.com>
2025-06-10 13:11:25 -0400

    add a message for empty previews

--- inc/class.admin.php
+++ inc/class.admin.php
@@ -645,22 +645,22 @@ class SimpleTags_Admin
 		wp_register_style('st-admin', STAGS_URL . '/assets/css/admin.css', array(), STAGS_VERSION, 'all');
 		wp_register_style('st-admin-global', STAGS_URL . '/assets/css/admin-global.css', array(), STAGS_VERSION, 'all');
 
-
-		// Register Tooltip
-		wp_register_script(
+		// Register tooltip
+        wp_register_script(
             'taxopress-admin-tooltip',
             STAGS_URL . '/assets/lib/tooltip/js/tooltip.min.js',
             ['jquery'],
             STAGS_VERSION
         );
 
-        wp_register_style(
+		wp_register_style(
             'taxopress-admin-tooltip',
             STAGS_URL . '/assets/lib/tooltip/css/tooltip.min.css',
             [],
             STAGS_VERSION
         );
 
+		//enqueue tooltip
 		wp_enqueue_script('taxopress-admin-tooltip');
 		wp_enqueue_style('taxopress-admin-tooltip');
 
