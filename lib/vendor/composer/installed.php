97be663eb034fdbabae78513fdcf19a36aac9965
Adedeji Adesina <adesinafelix98@gmail.com>
2026-05-04 18:47:22 +0100

    remove scripts already present in dev-workspace

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'bc4a63a97f7a36e43c7cb08a314aa1e1889bb79b',
+        'reference' => '47eef8a3181253816903b0214f9c01cb83ad4c82',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'bc4a63a97f7a36e43c7cb08a314aa1e1889bb79b',
+            'reference' => '47eef8a3181253816903b0214f9c01cb83ad4c82',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
