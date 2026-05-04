39ff04b8622643d2b3003cb77bd74099addb49f8
Adedeji Adesina <adesinafelix98@gmail.com>
2026-05-04 10:48:07 +0100

    migrate to shared dev workspace and remove duplicate Composer dependencies

---
+++ lib/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '61d575a709c5b781fa30b960bb92ee4121a1c498',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '61d575a709c5b781fa30b960bb92ee4121a1c498',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
